9756e9bbae5a20f132e68ab5d81d0ab97cac1627
5820-李孟华 <limenghua@wondersgroup.com>
2022-02-15 13:02:48 +0800

    OpenFeign日志

--- cloudalibaba-openFeign-consumer-8888/src/main/java/com/hua/CloudAlibabaOpenFeignConsumer8888Application.java
+++ cloudalibaba-openFeign-consumer-8888/src/main/java/com/hua/CloudAlibabaOpenFeignConsumer8888Application.java
@@ -1,9 +1,12 @@
 package com.hua;
 
+import feign.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Hello world!
@@ -14,5 +17,11 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class CloudAlibabaOpenFeignConsumer8888Application {
 	public static void main(String[] args) {
 		SpringApplication.run(CloudAlibabaOpenFeignConsumer8888Application.class, args);
+	}
+
+	@Bean
+	public Logger.Level feignLoggerLevel(){
+		// 开启openFeign的详细日志
+		return Logger.Level.FULL;
 	}
 }
